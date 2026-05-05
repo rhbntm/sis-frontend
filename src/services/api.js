274fd99a750db498284508b8a688347ee247feec
Brent Marcelo <ralphbrentm@gmail.com>
2026-05-05 22:44:00 +0800

    Update README.md

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://localhost/student_information_system/public/api";
+export const API_BASE = "http://student-information-system.test/api";
 
 // Create axios instance
 const api = axios.create({
